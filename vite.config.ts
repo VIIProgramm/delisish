d67c7ab323f61011b63451ab19ca698ba1d78c49
EvGenI191 <zema9com.com@gmail.com>
2023-10-31 20:42:30 +0300

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,13 @@
+import { resolve } from 'path'
+
 export default {
-  base: '/delisish/'
+  base: '/delisish/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        menu: resolve(__dirname, 'menu.html')
+      }
+    }
+  }
 }
