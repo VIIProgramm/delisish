48c57305ec3499b4dba6c948d8ce93e863b48f73
EvGenI191 <zema9com.com@gmail.com>
2023-11-02 02:10:43 +0300

    zap

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ export default {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        menu: resolve(__dirname, 'menu.html')
-        japane: resolve(__dirname, 'japane.html')
+        menu: resolve(__dirname, 'menu.html'),
+        japane: resolve(__dirname, 'japane.html'),
         chine: resolve(__dirname, 'chine.html')
         
       }
