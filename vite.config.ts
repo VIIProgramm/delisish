ae66b9011c41d76d64af3b9a48a83a3e9b5cc57c
EvGenI191 <zema9com.com@gmail.com>
2023-11-02 02:02:59 +0300

    config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,10 @@ export default {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        menu: resolve(__dirname, 'menu.html')
+        menu: resolve(__dirname, 'menu.html'),
+        menu: resolve(__dirname, 'japane.html'),
+        menu: resolve(__dirname, 'chine.html')
+        
       }
     }
   }
